d6a03acc8ad5435a24ad8ffc1cf00f5b9911fe05
degyves <degyves@gmail.com>
2021-10-23 09:18:07 -0500

    add scrolldown

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
-/*if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}*/
+var containerStyleDisplay = null;
+
+document.addEventListener('readystatechange', event => { 
+
+    // When HTML/DOM elements are ready:
+    if (event.target.readyState === "interactive") {   //does same as:  ..addEventListener("DOMContentLoaded"..
+    }
+
+    // When window loaded ( external resources are loaded too- `css`,`src`, etc...) 
+    if (event.target.readyState === "complete") {
+        containerStyleDisplay = document.querySelector('.container').style.display;
+    }
+});
+
+
+document.onscroll = function() {
+    if (window.innerHeight + window.scrollY >=
+        (document.body.scrollHeight -5)
+    ) {
+        document.querySelector('.container').style.display='none';
+    }else{
+        document.querySelector('.container').style.display=containerStyleDisplay;
+    }
+    console.log("");
+}
